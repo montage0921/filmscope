5f4cfb4a261a0e67e114fd2c552f188b64d9f90f
montage0921 <garyshi0921@gmail.com>
2026-01-21 19:40:55 -0500

    add theatre config file

---
+++ data/config.py
@@ -0,0 +1,21 @@
+THEATRE_WEBSITES ={
+    "tiff":{
+        "website":"https://www.tiff.net/calendar", # main entry
+        "showpage_xpath":"//h3[contains(@class, 'cardTitle')]//a", # each movie page's link
+        "excluded_tags":["footer"], 
+        "excluded_selectors":['[aria-label="Other Film Recommendations slider section"]','[id="openLegendModal"]'],
+        "button_clicked": """
+                            const button = document.querySelector('button[aria-label="Toggle show more showtimes"]');
+                            if (button) {
+                                button.click();
+                            }
+                            """
+    },
+    "paradise":{
+        "website":"https://paradiseonbloor.com/coming-soon/", # main entry
+        "showpage_xpath":"//a[@class='title']", # each movie page's link
+        "excluded_tags":["footer","header"], 
+        "excluded_selectors":[],
+        "button_clicked": ""
+    }
+}
