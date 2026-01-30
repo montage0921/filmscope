fdedf1caa6fced30e54d49e0dad0a82d536db617
montage0921 <garyshi0921@gmail.com>
2026-01-29 23:25:40 -0500

    add loading dot component

---
+++ frontend/src/Utility/LoadingDots.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { motion, type Variants } from "motion/react";
+
+function LoadingDots() {
+  return (
+    <motion.div
+      animate="pulse"
+      transition={{ staggerChildren: -0.2, staggerDirection: -1 }}
+      className="container"
+    >
+      <motion.div className="dot" variants={dotVariants} />
+      <motion.div className="dot" variants={dotVariants} />
+      <motion.div className="dot" variants={dotVariants} />
+      <StyleSheet />
+    </motion.div>
+  );
+}
+
+function StyleSheet() {
+  return (
+    <style>
+      {`
+            .container {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                gap: 20px;
+            }
+
+            .dot {
+                width: 20px;
+                height: 20px;
+                border-radius: 50%;
+                background-color: #ab76f5;
+                will-change: transform;
+            }
+            `}
+    </style>
+  );
+}
+
+const dotVariants: Variants = {
+  pulse: {
+    scale: [1, 1.5, 1],
+    transition: {
+      duration: 1.2,
+      repeat: Infinity,
+      ease: "easeInOut",
+    },
+  },
+};
+
+export default LoadingDots;
